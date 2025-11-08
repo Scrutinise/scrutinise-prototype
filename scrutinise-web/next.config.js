afbdf04f3379bcb7cbe44902ff3d95cd965c157a
Scrutinise <charles@scalablefinance.com>
2025-11-08 15:34:17 +0000

    REVERT: back to working 'next start'

--- scrutinise-web/next.config.js
+++ scrutinise-web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone'
+  // Remove experimental.serverComponentsExternalPackages — not needed for now
 }
 
 module.exports = nextConfig
