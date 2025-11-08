59585c8334546f71dc5351afe04e0b409140e87e
Scrutinise <charles@scalablefinance.com>
2025-11-08 15:54:30 +0000

    PRODUCTION: standalone server

--- scrutinise-web/next.config.js
+++ scrutinise-web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone'
-  // Remove experimental.serverComponentsExternalPackages — not needed for now
 }
 
 module.exports = nextConfig
